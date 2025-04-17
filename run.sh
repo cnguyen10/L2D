6cab6141e145df885f05184f2f530d84cc921241
Nguyen <cn0025@ulws070.surrey.ac.uk>
2025-04-17 20:45:18 +0100

    Add Flickr LDL dataset

--- run.sh
+++ run.sh
@@ -5,7 +5,4 @@ export CUDA_VISIBLE_DEVICES=${DEVICE_ID}
 # to build an image, use the following command
 # >> apptainer build apptainer.sif apptainer.def
 
-apptainer exec --nv --bind /sda2/datasets:/sda2/datasets apptainer.sif /app/venv/bin/python3 "main.py"
-apptainer exec --nv --bind /sda2/datasets:/sda2/datasets apptainer.sif /app/venv/bin/python3 "main.py" "hparams.epsilon_upper=[0.15, 0.15, 0.15, 0.15, 0.4]"
-apptainer exec --nv --bind /sda2/datasets:/sda2/datasets apptainer.sif /app/venv/bin/python3 "main.py" "hparams.epsilon_upper=[0.1, 0.1, 0.1, 0.1, 0.6]"
-apptainer exec --nv --bind /sda2/datasets:/sda2/datasets apptainer.sif /app/venv/bin/python3 "main.py" "hparams.epsilon_upper=[0.05, 0.05, 0.05, 0.05, 0.8]"
+python3 "main.py"
